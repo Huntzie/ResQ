f2aa1be59b88506699fb9e469c41849a565f633e
Heather <h.macdougal@live.co.uk>
2015-02-20 15:57:20 +0000

    Making it look better

--- ProLyfeQuoteTool/ProLyfeQuoteTool/MainWindow.xaml.cs
+++ ProLyfeQuoteTool/ProLyfeQuoteTool/MainWindow.xaml.cs
@@ -24,5 +24,32 @@ namespace ProLyfeQuoteTool
         {
             InitializeComponent();
         }
+
+        private void PART_CLOSE_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void PART_MINIMISE_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = System.Windows.WindowState.Minimized;
+        }
+
+        private void PART_MAXIMISE_RESTORE_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == System.Windows.WindowState.Normal)
+            {
+                this.WindowState = System.Windows.WindowState.Maximized;
+            }
+            else
+            {
+                this.WindowState = System.Windows.WindowState.Normal;
+            }
+        }
+
+        private void PART_TITLEBAR_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
     }
 }
